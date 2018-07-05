04fe2c5c36252ff3dae18d121ef7e80335c640c3
EC2 Default User <ec2-user@ip-172-31-17-110.us-east-2.compute.internal>
2018-07-05 15:37:22 +0000

    three tests one bad two ok

--- test_jenk.py
+++ test_jenk.py
@@ -1,4 +1,24 @@
 def sum(num):
     return num + 3
+
 def test_sum():
     assert sum(2) == 5
+
+
+
+
+def dev(num):
+    return num/2
+
+def test_dev():
+    assert dev(4) == 3
+
+
+
+
+def sub(num):
+    return num - 2
+
+def test_sub():
+    assert sub(4) == 2
+
